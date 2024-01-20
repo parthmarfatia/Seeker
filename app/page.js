5fded67c8f59a2d62a3021a9b91ed28ff946a7bc
Anayat Khan <anayat0khan@gmail.com>
2024-01-20 18:58:42 +0530

    Shadcn/ui setup

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import SwipeCard from "@/components/SwipeCard";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-full">
-      <SwipeCard />
+      <Button variants="primary">
+        <SwipeCard />
+      </Button>
     </div>
   );
 }
