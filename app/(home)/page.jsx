6522b50737ab4780b339e5c9f952208bf037f31b
Anayat Khan <anayat0khan@gmail.com>
2024-01-20 20:13:04 +0530

    Mode Toggle

---
+++ app/(home)/page.jsx
@@ -0,0 +1,13 @@
+import SwipeCard from "@/components/SwipeCard";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-full">
+        <Button variants="primary">
+          <SwipeCard />
+        </Button>
+      </div>
+    </>
+  );
+}
---
+++ components/nav/navbar.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { Sun, Moon } from "lucide-react";
+import { Button } from "../ui/button";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  return (
+    <div className="flex flex-row items-center justify-between px-3 py-4 sm:px-6 md:px-12">
+      <div className="items-start space-x-5">
+        <Link
+          className="flex flex-row items-center space-x-4 text-2xl font-extrabold tracking-tight text-secondary-900 dark:text-secondary-50"
+          href="/"
+        >
+          Seeker
+        </Link>
+      </div>
+      <ul className="flex flex-row items-center justify-between space-x-2">
+        {/* Theme Toggle */}
+        <li>
+          {currentTheme === "dark" ? (
+            <Button variant="ghost" onClick={() => setTheme("light")}>
+              <Sun />
+            </Button>
+          ) : (
+            <Button variant="ghost" onClick={() => setTheme("dark")}>
+              <Moon />
+            </Button>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
