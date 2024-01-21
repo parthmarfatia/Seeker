5c1f1246cc6a5419658a11e97191c313463e120f
Anayat Khan <anayat0khan@gmail.com>
2024-01-21 23:16:58 +0530

    Update content swiper

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body className={inter.className} >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
