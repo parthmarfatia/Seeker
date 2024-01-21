2a913c6cd0e18ce5627a16fd78df9f496c6f9d54
Anayat Khan <anayat0khan@gmail.com>
2024-01-21 23:10:40 +0530

    Update content swiper

--- app/swipe/page.jsx
+++ app/swipe/page.jsx
@@ -20,7 +20,7 @@ function SwipePage() {
   const [companies, setCompanies] = useState([]);
   const [isMatched, setIsMatched] = useState(false);
   const [count, setCount] = useState(0);
-  const [currentIndex, setCurrentIndex] = useState(companies.length - 1);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const currentIndexRef = useRef(currentIndex);
 
   const childRefs = useMemo(
@@ -73,64 +73,67 @@ function SwipePage() {
   }, []);
 
   useEffect(() => {
+    console.log(lastDirection)
     if (lastDirection === "right") {
+      
       setCount((prev) => prev + 1);
     }
-  }, [lastDirection]);
+    setIsMatched(false)
+  }, [lastDirection, currentIndex]);
 
   useEffect(() => {
-    if (count === 1) {
+    if (count === 3) {
       setIsMatched(true);
-    }
+     
+    } 
+    
   }, [count]);
 
   return (
     <div id="swipe-container">
-      {isMatched ? (
+      {isMatched && (
         <div>
           <Alert>
-            <PartyPopper className="h-4 w-4" />
+            <PartyPopper className="w-4 h-4" />
             <AlertTitle className="text-green-500">
               You have Matched!
             </AlertTitle>
           </Alert>
         </div>
-      ): (
-        " "
       )}
       <div className="cardContainer">
         {companies.map((character, index) => (
           <TinderCard
             ref={childRefs[index]}
             className="swipe"
-            key={character.company}
+            key={index}
             onSwipe={(dir) => swiped(dir, character.company, index)}
             onCardLeftScreen={() => outOfFrame(character.company, index)}
           >
             <div className="card">
               <img className="card-img " src="/bg-img.png" alt="bg-img" />
-              <h1 className="text-4xl font-bold pt-4 text-gray-800 font-sans hover:font-serif">
+              <h1 className="pt-4 font-sans text-4xl font-bold text-gray-800 hover:font-serif">
                 {character.company}
               </h1>
-              <p className="text-md pt-2 font-semibold">{character.title}</p>
+              <p className="pt-2 font-semibold text-md">{character.title}</p>
               <div className="pt-10">
                 <Dialog>
                   <DialogTrigger asChild>
                     <Button size="lg">Job Description</Button>
                   </DialogTrigger>
                   <DialogContent className="mx-auto flex w-[calc(100%_-_48px)] flex-col items-center justify-center rounded-2xl bg-white px-6 py-4 sm:max-w-[534px]">
-                    <DialogHeader className="mt-16 flex flex-col gap-4">
+                    <DialogHeader className="flex flex-col gap-4 mt-16">
                       <DialogTitle className="text-center text-[28px] font-bold leading-9 text-zinc-900">
                         Please read the Job Description Carefully
                       </DialogTitle>
-                      <DialogDescription className="rounded-xl bg-gray-50 p-3">
-                        <p className="text-center leading-tight text-gray-800 text-base text-balance">
+                      <DialogDescription className="p-3 rounded-xl bg-gray-50">
+                        <p className="text-base leading-tight text-center text-gray-800 text-balance">
                           {character.description}
                         </p>
                       </DialogDescription>
                     </DialogHeader>
 
-                    <div className="mt-10 flex items-start justify-between gap-4 self-stretch sm:justify-end">
+                    <div className="flex items-start self-stretch justify-between gap-4 mt-10 sm:justify-end">
                       <DialogClose asChild>
                         <button className="rounded-lg border border-blue-500 px-4 py-2.5 text-center text-sm font-medium leading-tight text-blue-500">
                           Cancel
@@ -151,26 +154,36 @@ function SwipePage() {
 
       <div className="flex">
         <div className="space-x-3">
-          <Button variant="destructive" className={`${!canSwipe}`} onClick={() => swipe("left")}>
+          <Button
+            variant="destructive"
+            className={`${!canSwipe}`}
+            onClick={() => swipe("left")}
+          >
             Swipe left!
           </Button>
-          <Button variant="outline" className={`${!canGoBack}`} onClick={() => goBack()}>
+          <Button
+            variant="outline"
+            className={`${!canGoBack}`}
+            onClick={() => goBack()}
+          >
             Undo swipe!
           </Button>
-          <Button className={`${!canSwipe}`} onClick={() => swipe("right")}>Swipe right!</Button>
+          <Button className={`${!canSwipe}`} onClick={() => swipe("right")}>
+            Swipe right!
+          </Button>
         </div>
       </div>
       {lastDirection ? (
         <h2
           key={lastDirection}
-          className="text-gray-800 text-md font-semibold font-sans hover:font-serif"
+          className="font-sans font-semibold text-gray-800 text-md hover:font-serif"
         >
           You swiped {lastDirection}
         </h2>
       ) : (
         <div>
           <Alert>
-            <Terminal className="h-4 w-4" />
+            <Terminal className="w-4 h-4" />
             <AlertTitle>Heads up!</AlertTitle>
             <AlertDescription>
               Swipe a card or press a button to get Restore Card button visible!
